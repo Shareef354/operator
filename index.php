d68e04dd453e4911ac51caced8d8ac29a3e2f971
shareef354 <zuhashmohd@gmail.com>
2022-01-25 14:53:29 +0300

    Bug Resolved

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html lang="en">
 
 <title>operator</title>
-	<link rel="stylesheet" href="operator.java">
+	<link rel="stylesheet" href="operator.php">
 
 <body>
 
 <h1>this is a home page welcome to the operator</h1>
-<button type="button">click here to enter</button>
+<button type="button"  href="operator.php">click here to enter</button>
 
 
 </body>
---
+++ operator.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<title>operator</title>
+	<!-- <link rel="stylesheet" href="operator.php"> -->
+<body>
+
+<form>
+    <input type="number" name="number_one" placeholder="Enter first number">Number 1:</input>
+    <input type="number" name="number_two" placeholder="Enter second number">Number 2:</input>
+</form>
+
+<button type="button"  href="process.php">Sum</button>
+
+
+</body>
+</html>
